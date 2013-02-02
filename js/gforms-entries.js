84f9125fd6d84150b18c30ee2f896f4bcd964223
Benjamin Moody <emailme@benjaminmoody.com>
2013-02-02 15:14:03 -0500

    Removed delete files option checkbox from entries in trash. File delete option is set when entry is first sent to the trash NOT perm deleted

--- js/gforms-entries.js
+++ js/gforms-entries.js
@@ -5,16 +5,18 @@ jQuery(document).ready(function(){
 	var checkbox_value	= null;
 	var publish_action	= null;
 	
-	file_checkbox = '<div style="padding:10px 10px 10px 0;"><input id="prso_fineup_delete_uploads" type="checkbox" onclick="" name="prso_fineup_delete_uploads"><label for="prso_fineup_delete_uploads"> Delete Fine Uploader Files</label></div>';
+	file_checkbox = prso_gforms_pluploader.input_field_html;
 	
 	//Check if we are on the trash edit page
 	publish_action = jQuery('div#publishing-action .button-primary');
 	
-	//Prepend checkbox html above trash link
-	jQuery('div#major-publishing-actions').prepend(file_checkbox);
+	console.log(prso_gforms_pluploader);
 	
 	//If NOT trash edit view
 	if( publish_action.length !== 0 ) {
+		
+		//Prepend checkbox html above trash link
+		jQuery('div#major-publishing-actions').prepend(file_checkbox);
 		
 		//Remove the default onclick
 		jQuery('a.submitdelete').removeAttr('onclick');
@@ -22,9 +24,9 @@ jQuery(document).ready(function(){
 		//Intercept trash click
 		jQuery('a.submitdelete').click(function(event){
 			//Warn user that not deleting files unless checbox is checked
-			if( !jQuery('#prso_fineup_delete_uploads').is(':checked') ) {
+			if( !jQuery('#prso_pluploader_delete_uploads').is(':checked') ) {
 				
-				if( !confirm(prso_gforms_fineup.file_delete_message) ) {
+				if( !confirm(prso_gforms_pluploader.file_delete_message) ) {
 					event.preventDefault();
 				} else {
 					//Carry out gform default actions
@@ -38,13 +40,13 @@ jQuery(document).ready(function(){
 			
 		})
 		
-	}
-	
-	//Get delete file meta for this entry and set the delete checkbox
-	if( prso_gforms_fineup.file_delete_meta === 'checked' ) {
-		jQuery('#prso_fineup_delete_uploads').attr('checked', 'checked');
-	} else {
-		jQuery('#prso_fineup_delete_uploads').removeAttr('checked');
+		//Get delete file meta for this entry and set the delete checkbox
+		if( prso_gforms_pluploader.file_delete_meta === 'checked' ) {
+			jQuery('#prso_pluploader_delete_uploads').attr('checked', 'checked');
+		} else {
+			jQuery('#prso_pluploader_delete_uploads').removeAttr('checked');
+		}
+		
 	}
 	
 	//Carry out gform default actions
